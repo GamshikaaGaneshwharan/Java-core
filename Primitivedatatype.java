e3550072004d410654e45021ac5ec4532bec93a6
GamshikaaGaneshwharan <ganeshtharani21@gmail.com>
2026-02-26 10:34:28 +0530

    Add initial commit

---
+++ Primitivedatatype.java
@@ -0,0 +1,60 @@
+class Primitivedatatype{
+	public static void main(String args[]){
+		Byte b1;
+		b1=12;
+		System.out.println("Byte: "+b1);
+		
+		short s1;
+		s1=32000;
+		System.out.println("Byte: " +s1);
+		
+		int i;
+		i=65022;
+		System.out.println("Int: "+i);
+		
+		long l;
+		l=639937739L;
+		System.out.println("long: " +1);
+		
+		Float f1;
+		f1=25.6f;
+		System.out.println("Float: " +f1);
+		
+		double db;
+		db=86;
+		System.out.println("Double: "+db);
+		
+		boolean bool;
+		bool=true;
+		System.out.println("Char: "+bool);
+		
+		char ch;
+		ch='A';
+		System.out.println("Char: " +ch);
+		
+		char ch1;
+		ch1=65;
+		System.out.println("Char: "+ch1);
+		
+		char ch2;
+		ch2=0101;
+		System.out.println("Char: "+ch2);
+		
+		char ch3;
+		ch3=0x41;
+		System.out.println("Char: "+ch3);
+		
+		char ch4;
+		ch4=0b100001;
+		System.out.println("Char: "+ch4);
+		
+		char ch5;
+		ch5='\u0041';
+		System.out.println("Char: "+ch5);
+	}
+}
+		
+		
+
+
+		
