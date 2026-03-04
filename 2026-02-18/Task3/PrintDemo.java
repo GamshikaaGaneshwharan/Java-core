e0ddabc43b80617fcd4804a5a7b5d2f1ae76edf5
GamshikaaGaneshwharan <ganeshtharani21@gmail.com>
2026-03-04 16:20:32 +0530

    Create PrintDemo.java

---
+++ 2026-02-18/Task3/PrintDemo.java
@@ -0,0 +1,18 @@
+class printDemo {
+	public static void main(String args[]) {
+		System.out.println("My name is Gamshikaa");//using println
+		System.out.println("My fathr name is Ganeshwharan");
+		System.out.println("Myage is 22");
+		
+		System.out.println();
+		
+		System.out.print("My name is Gamshikaa");//Using print
+		System.out.print("My father name is Ganeshwharan");
+		System.out.print("My age is 22");
+		
+		//Using print with \n
+		System.out.print("My name is Gamshikaa\n");
+		System.out.print("My father name is Ganeshwharan\n");
+		System.out.print("My age is 22\n");
+	}
+}
