2c20b1385269aed0c0197bfc86443297e46f41fb
GamshikaaGaneshwharan <ganeshtharani21@gmail.com>
2026-03-05 11:59:12 +0530

    Add java files

---
+++ 2026-03-05/Task24/NestedForLoop.java
@@ -0,0 +1,13 @@
+class NestedForLoop{
+	public static void main(String args[]){
+		for(int x=1; x<=3; x++){
+			for(int y=1; y<=5; y++)
+				System.out.print(y);
+			System.out.println(x);
+		}
+	}
+}
+		
+			
+		
+	
---
+++ 2026-03-05/Task26/NestedDowhileLoop.java
@@ -0,0 +1,18 @@
+class NestedDowhileLoop{
+	public static void main(String args[]){
+		
+		int y=1;
+		
+		do{
+			int x =1;
+			
+			do{
+				System.out.print(x);
+				x++;
+			}while(x<=3);
+			
+			System.out.println(y);
+			y++;
+		}while (y<=5);
+	}
+}
