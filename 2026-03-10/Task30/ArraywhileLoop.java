871012a621233a9de621314b6a87a2cd26f23647
GamshikaaGaneshwharan <ganeshtharani21@gmail.com>
2026-03-10 11:02:30 +0530

    Array basics

---
+++ 2026-03-10/Task30/ArraywhileLoop.java
@@ -0,0 +1,11 @@
+class ArraywhileLoop{
+	public static void main(String args[]){
+		int[]y= {10,20,30,40,50};
+		
+		int i=0;
+		while(i<y.length){
+			System.out.println(y[i]);
+			i++;
+		}
+	}
+}
---
+++ 2026-03-10/Task31/ArraywhileloopReverse.java
@@ -0,0 +1,11 @@
+class ArraywhileloopReverse{
+	public static void main(String args[]){
+		int[]y={10,20,30,40,50};
+		int i=y.length-1;
+		while(1>=0){
+			System.out.println(y[i]);
+			i--;
+		}
+	}
+}
+
